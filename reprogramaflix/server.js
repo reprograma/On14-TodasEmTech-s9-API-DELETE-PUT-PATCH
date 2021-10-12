1b181f545ed022f34182d108bdab496d58fd7ded
Ananda Souto <88149291+ansndsc@users.noreply.github.com>
2021-10-12 16:00:13 -0300

    atualiza atividade feita em aula

--- reprogramaflix/server.js
+++ reprogramaflix/server.js
@@ -5,3 +5,4 @@ const app = require("./src/app") //chamando o arquivo app
 app.listen(7070, ()=>{
     console.log("alo doçura? to na porta 7070")
 })
+
--- reprogramaflix/src/controllers/filmesController.js
+++ reprogramaflix/src/controllers/filmesController.js
@@ -1,74 +1,66 @@
-//controller vai ter a logica
-
-//chamando o json de filmes
-const filmesJson = require("../models/filmes.json")
-
+const filmesJson = require("../models/filmes.json"); //../para sair da pasta atual. O controller vai conter todas as informarções contidas no filmes.json
 
 //função getAll retorna todos os filmes
 const getAll = (request, response) => {
-
+    
     response.status(200).json([
         {
-            "filmes": filmesJson
+            "filmes": filmesJson,            
         }
-    ])
+    ]);
 }
 
 
-//função getById retorna um filme de um id especifico
-const getById = (request, response)=>{
-    let idRequest = request.params.id
-    let idEncontrado = filmesJson.find(filme => filme.id == idRequest)
+const getById = (request, response) => {
+    let idRequest = request.params.id;
 
-    response.status(200).send(idEncontrado)
+    let idEncontrado = filmesJson.find(filme => filme.id == idRequest);
+
+    response.status(200).send(idEncontrado);
 }
 
-const createMovie = (request, response)=>{
-    let body = request.body
+const createMovie = (request, response) => {
+    let body = request.body;
 
     let novoFilme = {
-        id: (filmesJson.length)+1,
+        id: (filmesJson.length) + 1,
         Title: body.Title,
         Plot: body.Plot
     }
 
-    filmesJson.push(novoFilme)
-
+    filmesJson.push(novoFilme);
     response.status(201).json(
         [
             {
-                "mensagem":"filme cadastrado com sucesso",
-                novoFilme
+            "mensagem":"filmes cadastrado com sucesso.", novoFilme
+            
             }
         ]
     )
-
 }
 
-const updateTitle = (request, response)=>{
-    const idRequest = request.params.id 
-    let novoTitulo = request.body.Title
+const updateTitle = (request, response) => {
+    const idRequest = request.params.id;
+    let novoTitulo = request.body.Title;
 
-    filmeFiltrado = filmesJson.find(filme => filme.id == idRequest)
+    filmeFiltrado = filmesJson.find(filme => filme.id == idRequest);
 
     filmeFiltrado.Title = novoTitulo
 
     response.status(200).json(
         [
             {
-                "mensagem": "filme atualizadocom sucesso",
-                filmeFiltrado
+                "mensagem": "Filme atualizado com sucesso.", filmeFiltrado
             }
         ]
     )
 }
 
+//exportando todas as funções do controller para ser usada no filmesRoutes.js
 
-
-//exportando todas os funções do controller para ser usada no filmesRoutes.js
 module.exports = {
     getAll,
     getById,
     createMovie,
     updateTitle
-}
+} //me possibilita usar essas funções fora do arquivo, em qualquer lugar do meu código.
