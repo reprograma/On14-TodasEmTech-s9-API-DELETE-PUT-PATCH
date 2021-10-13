18c9efd96deb81c6f8df2f8ab0c7af270f4252b5
Ananda Souto <88149291+ansndsc@users.noreply.github.com>
2021-10-12 21:22:43 -0300

    Comenta o [GET]/filmes

--- paraolar/src/controllers/filmesController.js
+++ paraolar/src/controllers/filmesController.js
@@ -1,17 +1,17 @@
-const movieJson = require("../models/filmes.json");
+const moviesJson = require("../models/filmes.json"); //importa a Json de filmes
 
 const getAll = (request, response) => {
     
     response.status(200).send(
         [
             {
-                "movies": movieJson
+                "Movies": moviesJson
             }
         ]
     );
 
-}
+} //Cria a lógica que vai ser chamada para mostrar todos os filmes e armazena em uma variável. "Movies" é apenas o apelido que eu escolhi dar para o objeto que estou chamando.
 
 module.exports ={
     getAll
-}
+} //exporta as variáveis que contem as lógicas criadas aqui
