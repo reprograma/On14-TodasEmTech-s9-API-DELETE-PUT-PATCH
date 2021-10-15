20728e85d6b39f01f72122aa881a786d6cfa83b0
Ana Luiza Sampaio <sampaioaanaluiza@gmail.com>
2021-10-15 00:04:08 -0300

    filmes e series

--- reprogramaflix/src/controllers/seriesController.js
+++ reprogramaflix/src/controllers/seriesController.js
@@ -5,6 +5,18 @@ const getAll = (req, res)=>{
     res.status(200).send(seriesJson)
 }
 
+const getByGender = (req, res)=>{
+    const generoRequest = req.query.genero
+
+    const seriesFiltradas = seriesJson.filter( serie =>
+        serie.genre.toString().includes(generoRequest)
+    )
+
+    res.status(200).send(seriesFiltradas)
+
+}
+
 module.exports ={
-    getAll
+    getAll,
+    getByGender
 }
