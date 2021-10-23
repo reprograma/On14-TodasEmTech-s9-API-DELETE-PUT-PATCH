5b4121ed98895e4a5d46aa0938528cbe6d79cf29
Ananda Souto <88149291+ansndsc@users.noreply.github.com>
2021-10-22 21:40:24 -0300

    "Adiciona app.js de atividade para o lar"

--- paraolar/src/app.js
+++ paraolar/src/app.js
@@ -4,7 +4,7 @@ const cors = require("cors"); //importa o cors
 
 const moviesRoute = require("./routes/filmesRoutes"); //importa as rotas criadas nas Routes e atribuo a uma variável
 const tvShowRoute = require("./routes/tvShowRoutes");
-
+const allRoutes = require("./routes/allRoutes");
 const app = express(); //executa o express e coloca na variável app
 
 app.use(cors()); //executa o cors
@@ -12,5 +12,6 @@ app.use(express.json());
 
 app.use("/movies", moviesRoute); // "//movies" é a rota raíz que criei para chamar o "moviesRoute", variável que criei para armazenar o "filmesRoutes".
 app.use("/tvshow", tvShowRoute); //cria a rota raíz para tv show
+app.use("/assistir", allRoutes);
 
 module.exports = app; // exporta as rotas raízes criadas aqui
