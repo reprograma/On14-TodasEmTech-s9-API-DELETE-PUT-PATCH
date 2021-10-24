1e92ac77279ec82b49af74d6f718cb150f65fdbd
Edilainesds <edilaine45@yahoo.com.br>
2021-10-23 22:49:57 -0300

    entrega semana9

---
+++ paraolar/src/controllers/seriesController.js
@@ -0,0 +1,24 @@
+const seriesJson = require("../models/series.json")
+
+const getAll = (req, res)=>{
+    res.status(200).send(seriesJson)
+}
+
+const getByGender = (req, res)=>{
+    const generoRequest = req.query.genero
+
+
+const seriesFiltradas = seriesJson.filter(serie =>
+    serie.genre.toString().includes(generoRequest)
+
+)
+
+res.status(200).send(seriesFiltradas)
+
+}
+
+module.exports ={
+  getAll,
+  getByGender
+
+}
