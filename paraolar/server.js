6ec68f03e1172a9e8a8195be0a8397839a3a9f01
Jeniffer Alves <86132904+Jhenial@users.noreply.github.com>
2021-10-25 23:32:15 -0300

    Delete paraolar directory

--- paraolar/server.js
+++
@@ -1,9 +0,0 @@
-//Iniciando o Projeto ReprogramaFlix - Criando minha API
-//Criando a minha porta 8090
-
-const app = require ("./src/app")
-const PORT = 8090
-
-app.listen(PORT, ()=>{
-  console.log(`Alô, Prof Ana Lu? to na porta: ${PORT}`)
-  })
